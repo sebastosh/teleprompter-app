cb5268fef151edb261aade44e29e5c32b13ef706
sebastosh <sebastosh@gmail.com>
2019-06-17 15:59:54 -0400

    added seed file

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+
+const speakersURL = "http://localhost:3000/speakers"
+
+fetch(speakersURL)
+  .then(res => res.json())
+  .then(res => res.map(speaker => speaker.name))
+  .then(speakers => console.log(speakers));
+
+// Create New Speaker
+const enterForm = document.getElementById('enter-speaker')
+
+  enterForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const speakerName = event.target.name.value
+    const speakerTitle = event.target.title.value
+
+    const userData = {
+      name: speakerName,
+      title: speakerTitle
+    }
+
+
+
+
+
+
+    console.log(speakerName, speakerTitle); //
+
+  })
+
 document.addEventListener('DOMContentLoaded', () => {
-console.log("Hi John, just want you to know that the DOM is LOADED"); //
+  console.log("Hi John, just want you to know that the DOM is LOADED"); //
 
 // const signInDiv = document.createElement('div')
 // const signInForm = document.createElement('form')
